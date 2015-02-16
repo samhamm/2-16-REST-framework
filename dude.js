cc3c4dd7d9e4fa3e39fb4bb03fb47bb1d712f4c0
samhamm <samhamm@gmail.com>
2015-02-16 13:02:49 -0800

    more framework for the framework; need to move some things around

--- dude.js
+++ dude.js
@@ -6,36 +6,42 @@
 
 'use strict';
 
-require('./server.js');
+var server = require('./server.js');
 require('./lib/router.js');
-var event = require('events').EventEmitter;
 
 var dude = exports = module.exports = {};
 
-dude.at = function(location, data) {
-  console.log("at: The Dude POSTs")
+dude.at = function(location, stuff) {
+  console.log("at: The Dude POSTs");
 };
 
 dude.read = function(location){
-  console.log("read: The Dude GETs")
+  console.log("read: The Dude GETs");
+  // location = 'fart';
+  // server.id = location;
+  // console.log("The location passed into dude.read is " + server.id);
+  // router.req.method = 'GET';
 };
 
-dude.oppress = function(location, data) {
-  console.log("replace: The Dude PUTs")
+dude.oppress = function(location, newStuff) {
+  console.log("replace: The Dude PUTs");
 };
 
-dude.mix = function(location, data) {
-  console.log("mix: The Dude PATCHes")
+dude.mix = function(location, otherStuff) {
+  console.log("mix: The Dude PATCHes");
 };
 
 dude.trash = function(location){
-  console.log("trash: The Dude DELETEs")
+  // location = 'fuck';
+  server.id = location;
+  server.req = 'DELETE';
+  console.log("trash: The Dude DELETEs the route called " + server.id);
 };
 
 
 
-// dude.at(location).place(data) is the same as HTTP POST
+// dude.at(location).place(stuff) is the same as HTTP POST
 // dude.read(location) is the same as HTTP GET
-// dude.oppress(location).with(newData) is the same as HTTP PUT
-// dude.mix(location).with(otherData) is the same as HTTP PATCH
+// dude.oppress(location).with(newStuff) is the same as HTTP PUT
+// dude.mix(location).with(otherStuff) is the same as HTTP PATCH
 // dude.trash(location) is the same as HTTP DELETE
